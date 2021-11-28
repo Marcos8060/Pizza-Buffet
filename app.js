b6213a2d8bbe0fa6bfee2faa78d1bc39b0c16cac
Marcos Ochieng <marcosgav80@gmail.com>
2021-11-28 14:29:07 +0300

    added the delivery functionality

--- app.js
+++ app.js
@@ -77,3 +77,48 @@ function changeUnits(action,id){
     })
     updateCart();
 }
+
+// delivery functionality
+$(document).ready(function(){
+    $('#form').submit(function(e){
+        e.preventDefault();
+
+        // clear alert
+        clearAlert();
+
+        var nameInput = $('input#name').val();
+        var locationInput = $('input#location').val();
+
+        if(nameInput && locationInput){
+            // create element
+            const h5 = document.createElement('h5');
+            h5.className = 'alert alert-success';
+            h5.appendChild(document.createTextNode(`Thank you ${nameInput}, your order will be delivered to ${locationInput}`));
+            const searchContainer = document.querySelector('.searchContainer');
+            const search = document.querySelector('.search');
+            searchContainer.insertBefore(h5,search);
+        }else{
+            // create element
+            const h5 = document.createElement('h5');
+            h5.className = 'alert alert-danger';
+            h5.appendChild(document.createTextNode(`Please fill in all the required fields`));
+            const searchContainer = document.querySelector('.searchContainer');
+            const search = document.querySelector('.search');
+            searchContainer.insertBefore(h5,search);
+        }
+
+        setTimeout(()=>{
+            clearAlert();
+        },4000);
+        
+        $('input#name').val('');
+        $('input#location').val('');
+
+    })
+})
+function clearAlert(){
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert){
+        currentAlert.remove();
+    }
+}
